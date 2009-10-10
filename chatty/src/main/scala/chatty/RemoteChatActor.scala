b1ea2c0e3fdab57be33430090169bbb2bf480288
Øyvind Bø Syrstad <oyvind.bo.syrstad@gmail.com>
2009-10-11 01:26:49 +0200

    Test commit

---
+++ chatty/src/main/scala/chatty/RemoteChatActor.scala
@@ -0,0 +1,11 @@
+package chatty
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: nixx
+ * Date: Oct 10, 2009
+ * Time: 10:49:53 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class RemoteChatActor
---
+++ chatty/src/main/scala/swing/MainPanel.scala
@@ -0,0 +1,11 @@
+package swing
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: nixx
+ * Date: Oct 11, 2009
+ * Time: 1:24:10 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class MainPanel
